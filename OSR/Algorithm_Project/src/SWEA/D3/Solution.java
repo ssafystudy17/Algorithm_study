284462407b3e745ed31b4e2fbf575aa3cb411eae
OhSeongRak <osr0907@gmail.com>
2023-02-04 22:42:49 +0900

    [SWEA] 양식

--- OSR/Algorithm_Project/src/SWEA/D3/Solution.java
+++ OSR/Algorithm_Project/src/SWEA/D3/Solution.java
@@ -1,5 +1,20 @@
 package SWEA.D3;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int N = sc.nextInt();
+			int[] arr = new int[N];
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = sc.nextInt();
+			}
+
+//			System.out.printf("$%d %d%n", test_case, 0);
+		}
+	}
 }
