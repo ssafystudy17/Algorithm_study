5306a94d07ebde234ee950732f0ba95b47a12c4f
OhSeongRak <osr0907@gmail.com>
2023-02-26 16:41:56 +0900

    Main

---
+++ OSR/Algorithm_Project/src/Main.java
@@ -0,0 +1,26 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br;
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		input();
+		sol();
+	}
+
+	private static void sol() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void input() throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+
+	}
+}
